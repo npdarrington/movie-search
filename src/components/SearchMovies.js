89c9324fef2b0d26c6be28d063f971403a6fb0f5
Nathan Darrington <npdarrington@icloud.com>
2020-09-28 12:02:46 -0600

    Added query and useQuery states for when a user is searching rather than hard coded value

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function SearchMovies() {
+  const [query, setQuery] = useState('');
+
   const searchMovies = async (event) => {
     event.preventDefault();
-    const query = "Jurassic Park";
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
     try {
       const res = await fetch(url);
       const data  = await res.json();
+      console.log(data);
     } catch(err) {
       console.log(err);
     }
@@ -16,7 +18,10 @@ export default function SearchMovies() {
   return (
     <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">Movie Name</label>
-      <input type="text" name="query" id="query" placeholder="i.e. Jurassic Park"/>
+      <input type="text" name="query" id="query" 
+        placeholder="i.e. Jurassic Park" value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
       <button type="submit" className="button">Search</button>
     </form>
   )
